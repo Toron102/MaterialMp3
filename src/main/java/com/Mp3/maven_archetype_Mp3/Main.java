969dd51767fff3ca2ee4933f39ae7e252e0ef78b
Toron102 <gothic123455@gmail.com>
2023-03-06 18:25:05 +0100

    MaterialMp3

---
+++ src/main/java/com/Mp3/maven_archetype_Mp3/Main.java
@@ -0,0 +1,43 @@
+package com.Mp3.maven_archetype_Mp3;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+
+				@Override
+				public void handle(WindowEvent arg0) {
+
+					Platform.exit();
+					System.exit(0);
+					
+				}
+				
+			});
+			
+			
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/test/java/com/Mp3/maven_archetype_Mp3/AppTest.java
@@ -0,0 +1,20 @@
+package com.Mp3.maven_archetype_Mp3;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
